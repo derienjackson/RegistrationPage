67398b92376ab2e091202e8995adf5c355215c0b
Derien <112275650+derienjackson@users.noreply.github.com>
2022-12-17 13:58:00 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const dob = document.getElementById('dob');
+
+//Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control error'
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success';
+}
+
+//Check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+//Check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+//Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+//Check password
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+}
+
+//Check date of birth
+function checkDob(input) {
+    var dob = new Date(input.value);
+    var month_diff = Date.now() - dob.getTime();
+    var age_dt = new Date(month_diff);
+    var year = age_dt.getUTCFullYear();
+    var age = Math.abs(year - 1970);
+
+    if (age < 18) {
+        showError(input, 'Age of 18 or older is required');
+    }
+    else if (age >= 18) {
+        showSuccess(input);
+    }
+    else {
+        showError(input, 'Date of birth is required');
+    }
+
+}
+
+//Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//Event Listeners
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2, dob])
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+    checkDob(dob);
+})
